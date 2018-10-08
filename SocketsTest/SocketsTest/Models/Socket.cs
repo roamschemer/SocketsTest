59133e6090eedcc40db904f6011221e6bf32c708
放浪 軍師 <roamschemer@gmail.com>
2018-10-08 22:37:15 +0900

    完成

---
+++ SocketsTest/SocketsTest/Models/Socket.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Text;
+using Prism.Mvvm;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace SocketsTest.Models
+{
+    /// <summary>
+    /// Soket通信用
+    /// </summary>
+    class Socket : BindableBase
+    {
+        private string ipAddress = "192.168.1.1";
+        public string IpAddress
+        {
+            get => this.ipAddress;
+            set => this.SetProperty(ref this.ipAddress, value);
+        }
+        private int port = 9999;
+        public int Port
+        {
+            get => this.port;
+            set => this.SetProperty(ref this.port, value);
+        }
+        private bool isServer;
+        public bool IsServer
+        {
+            get => this.isServer;
+            set => this.SetProperty(ref this.isServer, value);
+        }
+        private bool isOpen;
+        public bool IsOpen
+        {
+            get => this.isOpen;
+            set => this.SetProperty(ref this.isOpen, value);
+        }
+        private string sendData;
+        public string SendData
+        {
+            get => this.sendData;
+            set => this.SetProperty(ref this.sendData, value);
+        }
+
+        public ObservableCollection<string> ActionInfo { get; } = new ObservableCollection<string>();
+
+        //ユーザ定義メンバ変数
+        private TcpListener tcplistener = null;
+        private TcpClient tcpClient = null;
+
+        //ストリーム
+        private NetworkStream networkStream = null;
+        private StreamReader streamReader = null;
+        private StreamWriter streamWriter = null;
+
+#pragma warning disable 4014 //Task.Runの同期と非同期が混同してるが別に間違いじゃないので警告回避。
+
+        /// <summary>
+        /// 通信開始
+        /// </summary>
+        public async void Open()
+        {
+            string methodName = (IsServer ? "Server" : "Client") + "Open";
+            try
+            {
+                IsOpen = true;
+                //サーバー側の挙動
+                if (IsServer)
+                {
+                    ActionInfo.Insert(0, DateTime.Now.ToString("hh:mm:ss") + "," + methodName + "," + "接続待機");
+                    tcplistener = new TcpListener(IPAddress.Any, Port);
+                    tcplistener.Start();
+                    Task.Run(async () =>
+                    {
+                        //クライアントの要求があったら、接続を確立する(接続があるかtcplistener.Stop()が実行されるまで待機する)
+                        await Task.Run(() => tcpClient = tcplistener.AcceptTcpClient());
+                        ActionInfo.Insert(0, DateTime.Now.ToString("hh:mm:ss") + "," + methodName + "," + "接続完了");
+                        networkStream = tcpClient.GetStream();
+                        streamReader = new StreamReader(networkStream, Encoding.UTF8);
+                        streamWriter = new StreamWriter(networkStream, Encoding.UTF8);
+                        Task.Run(() => Receive());
+                    });
+                }
+                //クライアント側の挙動
+                if (!IsServer)
+                {
+                    ActionInfo.Insert(0, DateTime.Now.ToString("hh:mm:ss") + "," + methodName + "," + "接続待機");
+                    //サーバーが見つかったら、接続を確立する(接続があるかタイムアウトまで待機する)
+                    await Task.Run(() => tcpClient = new TcpClient(IpAddress, Port));
+                    ActionInfo.Insert(0, DateTime.Now.ToString("hh:mm:ss") + "," + methodName + "," + "接続完了");
+                    networkStream = tcpClient.GetStream();
+                    streamReader = new StreamReader(networkStream, Encoding.UTF8);
+                    streamWriter = new StreamWriter(networkStream, Encoding.UTF8);
+                    Task.Run(() => Receive());
+                }
+            }
+            catch (Exception ex)
+            {
+                ActionInfo.Insert(0, DateTime.Now.ToString("hh:mm:ss") + "," + methodName + "," + ex.Message);
+                Close();
+                IsOpen = false;
+            }
+        }
+
+        /// <summary>
+        /// 接続終了
+        /// </summary>
+        public void Close()
+        {
+            string methodName = (IsServer ? "Server" : "Client") + "Close";
+            try
+            {
+                if (IsServer) tcplistener.Stop();
+                if (!IsServer)
+                {
+                    if ((tcpClient != null) && tcpClient.Connected)
+                    {
+                        tcpClient.Close();
+                        tcpClient.Dispose();
+                    }
+                }
+                if (networkStream != null)
+                {
+                    networkStream.Close();
+                    networkStream.Dispose();
+                }
+                tcplistener = null;
+                tcpClient = null;
+                networkStream = null;
+                ActionInfo.Insert(0, DateTime.Now.ToString("hh:mm:ss") + "," + methodName + "," + "接続終了");
+            }
+            catch (Exception ex)
+            {
+                ActionInfo.Insert(0, DateTime.Now.ToString("hh:mm:ss") + "," + methodName + "," + ex.Message);
+            }
+            IsOpen = false;
+        }
+
+        /// <summary>
+        /// 信号送信
+        /// </summary>
+        public void Send()
+        {
+            string methodName = (IsServer ? "Server" : "Client") + "Send";
+            try
+            {
+                byte[] tmp = Encoding.UTF8.GetBytes(SendData);
+                networkStream.Write(tmp, 0, tmp.Length);//送信 引数は（データ , データ書き込み開始位置 , 書き込むバイト数）
+                ActionInfo.Insert(0, DateTime.Now.ToString("hh:mm:ss") + "," + methodName + "," + SendData);
+            }
+            catch (Exception ex)
+            {
+                ActionInfo.Insert(0, DateTime.Now.ToString("hh:mm:ss") + "," + methodName + "," + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 信号受信(受信があるまで待機する)
+        /// </summary>
+        private async void Receive()
+        {
+            string methodName = (IsServer ? "Server" : "Client") + "Receive";
+            try
+            {
+                await Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        string res = null;
+                        byte[] data = new byte[256];
+                        string receiveData = string.Empty;
+                        int bytes = networkStream.Read(data, 0, data.Length);
+                        if (bytes == 0) break;
+                        res = Encoding.UTF8.GetString(data, 0, bytes);
+                        ActionInfo.Insert(0, DateTime.Now.ToString("hh:mm:ss") + "," + methodName + "," + res);
+                    }
+                    ActionInfo.Insert(0, DateTime.Now.ToString("hh:mm:ss") + "," + methodName + "," + "接続が遮断されました。");
+                    Close();
+                });
+            }
+            catch (Exception ex)
+            {
+                //遮断エラー(IOException(SocketExceptionじゃないのは何故…？))の場合はメッセージ不要
+                if (!(ex is IOException)) ActionInfo.Insert(0, DateTime.Now.ToString("hh:mm:ss") + "," + methodName + "," + ex.Message);
+            }
+        }
+
+#pragma warning restore 4014 //Task.Runの同期と非同期が混同してるが別に間違いじゃないので警告回避。
+
+    }
+}
---
+++ SocketsTest/SocketsTest/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,74 @@
+﻿using Prism.Navigation;
+using Reactive.Bindings;
+using System;
+using System.Linq;
+using System.Reactive.Disposables;
+using SocketsTest.Models;
+using Reactive.Bindings.Extensions;
+using System.Reactive.Linq;
+
+namespace SocketsTest.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase, IDisposable
+    {
+        //メモリリーク防止
+        private CompositeDisposable Disposable { get; } = new CompositeDisposable();
+
+        //情報
+        public ReactiveProperty<bool> ServerSwitch { get; private set; }
+        public ReactiveProperty<bool> SettingIsEnabled { get; private set; }
+        public ReactiveProperty<string> ServerSwitchInfo { get; private set; }
+        public ReactiveProperty<string> IpAddress { get; private set; }
+        public ReactiveProperty<bool> IPAddressSettingIsEnabled { get; private set; }
+        public ReactiveProperty<int> Port { get; private set; }
+        public ReactiveProperty<string> SendData { get; private set; }
+
+        // 挙動情報(エラーや通信履歴)
+        public ReadOnlyReactiveCollection<string> ActionView { get; }
+
+        // 開始終了
+        public ReactiveCommand OpenCommand { get; private set; }
+        public ReactiveCommand CloseCommand { get; private set; }
+        public ReactiveCommand SendCommand { get; private set; }
+
+        // Models.Socket
+        private Socket socket = new Socket();
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+
+            //ViweModel→Model 
+            this.ServerSwitch = ReactiveProperty.FromObject(socket, x => x.IsServer).AddTo(this.Disposable);
+            this.IpAddress = ReactiveProperty.FromObject(socket, x => x.IpAddress).AddTo(this.Disposable);
+            this.Port = ReactiveProperty.FromObject(socket, x => x.Port).AddTo(this.Disposable);
+            //ViewModel←Model
+            this.SettingIsEnabled = socket.ObserveProperty(x => x.IsOpen).Select(x => !x).ToReactiveProperty().AddTo(this.Disposable);
+            this.CloseCommand = socket.ObserveProperty(x => x.IsOpen).ToReactiveCommand().AddTo(this.Disposable);
+            this.OpenCommand = socket.ObserveProperty(x => x.IsOpen).Select(x => !x).ToReactiveCommand().AddTo(this.Disposable);
+            this.SendCommand = socket.ObserveProperty(x => x.IsOpen).ToReactiveCommand().AddTo(this.Disposable);
+            this.ActionView = socket.ActionInfo.ToReadOnlyReactiveCollection().AddTo(this.Disposable);
+            //ViewModel＝Model
+            this.SendData = socket.ToReactivePropertyAsSynchronized(x => x.SendData).AddTo(this.Disposable);
+
+            //ViweModel内
+            this.ServerSwitchInfo = this.ServerSwitch.Select(x => x ? "サーバー" : "クライアント").ToReactiveProperty().AddTo(this.Disposable);
+            this.IPAddressSettingIsEnabled = this.SettingIsEnabled.CombineLatest(this.ServerSwitch, (x, y) => x && !y) //クライアント且つCLOSE時のみIPアドレスは入力編集が可能
+                                                 .ToReactiveProperty().AddTo(this.Disposable);
+
+            //ボタン
+            OpenCommand.Subscribe(_ => socket.Open());
+            CloseCommand.Subscribe(_ => socket.Close());
+            SendCommand.Subscribe(_ => socket.Send());
+
+        }
+
+        /// <summary>
+        /// メモリリーク防止
+        /// </summary>
+        public void Dispose()
+        {
+            this.Disposable.Dispose();
+        }
+    }
+}
